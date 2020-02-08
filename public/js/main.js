3db0604f80304de050a22c27f8a1e605ed981ba7
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-02-08 22:58:24 +1000

    Restructure background handling

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,10 @@ var gameController = {}
 gameController.state = 'login'
 gameController.chatOpen = false
 
+function hideBackground() {
+    document.getElementById('background').style.display = "none"
+}
+
 socket.on('create board', function (boardData) {
     init()
     generate_board(boardData.tileData)
@@ -15,10 +19,11 @@ socket.on('lobby owner', function () {
 
 socket.on('start game', function () {
     document.getElementById('lobby').style.display = 'none'
+    gameController.state = 'board'
+    hideBackground()
 })
 
 socket.on('update player positions', function (positions) {
-    console.log(positions)
     for (var id in positions) {
         const position = positions[id]
         makeFox(position.x, position.y, id)
